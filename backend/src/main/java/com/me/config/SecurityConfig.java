158e5147612717f7ddcfccac0f52ea5ebcada38c
ADHD-Donkey <roden.gleb@gmail.com>
2026-05-06 18:11:24 +0300

    securityFilterChain is edited

--- backend/src/main/java/com/me/config/SecurityConfig.java
+++ backend/src/main/java/com/me/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,12 +21,15 @@ public class SecurityConfig
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http)
 	{
+		// Disable special csrf check and allow specific auth requests to be passed to backend without token.
+		// Other requests will require token.
 		http.csrf(AbstractHttpConfigurer::disable)
-		    .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**")
-		                                       .permitAll()
-		                                       .anyRequest()
-		                                       .authenticated()
-		    );
+		    .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		    .authorizeHttpRequests(auth -> auth
+		    .requestMatchers("/api/auth/register", "/api/auth/login")
+		    .permitAll()
+		    .anyRequest()
+		    .authenticated());
 
 		return http.build();
 	}
