004e52d6825ef4572ee4570a6e09e2059b92c6c9
ADHD-Donkey <roden.gleb@gmail.com>
2026-05-10 00:34:00 +0300

    vite config is added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,33 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+export default defineConfig(
+{
+    plugins: [react()],
+
+    server:
+    {
+        host: true,
+
+        port: 5173,
+
+        proxy:
+        {
+            '/api':
+            {
+                target: 'http://matchme-backend:8080',
+                changeOrigin: true,
+            },
+        },
+
+        watch:
+        {
+            usePolling: true,
+        },
+
+        hmr:
+        {
+            clientPort: 3000,
+        },
+    },
+});
