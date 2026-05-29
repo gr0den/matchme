3faa54168e7929d2ab4b615492986a80f78acc6e
ADHD-Donkey <roden.gleb@gmail.com>
2026-05-29 14:29:15 +0300

    fix

--- backend/src/main/java/com/me/entities/Genre.java
+++ backend/src/main/java/com/me/entities/Genre.java
@@ -12,13 +12,10 @@ import java.util.Set;
 @Setter
 public class Genre
 {
-
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Column(nullable = false, unique = true)
     private String name;
-
-
 }
