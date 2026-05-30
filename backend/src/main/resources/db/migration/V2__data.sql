8d5ccf465066037f620a5d8b6baab1def8947327
ADHD-Donkey <roden.gleb@gmail.com>
2026-05-30 18:04:51 +0300

    sql data

---
+++ backend/src/main/resources/db/migration/V2__data.sql
@@ -0,0 +1,38 @@
+INSERT INTO genres (genre)
+VALUES
+        ('Sci-Fi'),
+        ('Fantasy'),
+        ('Romance'),
+        ('Crime'),
+        ('Thriller'),
+        ('Horror'),
+        ('Memoir'),
+        ('Mystery'),
+        ('Non-Fiction'),
+        ('Tech');
+
+INSERT INTO interests (interest)
+VALUES
+        ('Engineering'),
+        ('Sport'),
+        ('Travelling'),
+        ('Entrepreneurship'),
+        ('Philosophy'),
+        ('Interest6'),
+        ('Interest7'),
+        ('Interest8'),
+        ('Interest9'),
+        ('Interest10');
+
+INSERT INTO intentions (intention)
+VALUES
+            ('Engineering'),
+            ('Sport'),
+            ('Travelling'),
+            ('Entrepreneurship'),
+            ('Philosophy'),
+            ('Intention1'),
+            ('Intention2'),
+            ('Intention3'),
+            ('Intention4'),
+            ('Intention5');
