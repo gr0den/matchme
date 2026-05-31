1619a1429d99b4a163a2f3081d241d67d4ef2d05
ADHD-Donkey <roden.gleb@gmail.com>
2026-05-31 13:07:50 +0300

    stage

--- backend/src/main/java/com/me/dto/request/profile/UserProfileRequestDto.java
+++ backend/src/main/java/com/me/dto/request/profile/UserProfileRequestDto.java
@@ -1,5 +1,6 @@
 package com.me.dto.request.profile;
 
+import com.me.entities.Genre;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
@@ -23,5 +24,11 @@ public class UserProfileRequestDto
 
     @NotEmpty(message = "Interests set cannot be empty")
     @Size(min = 5, max = 10, message = "Amount of selected interests must be between 5 and 10")
-    private Set<Long> interestIds;
+    private Set<Long> interests;
+
+    @NotEmpty(message = "Genres set cannot be empty")
+    @Size(min = 1, max = 10, message = "Amount of selected genres should be at least 1")
+    private Set<Genre> genres;
+
+    @
 }
