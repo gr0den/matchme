06554bf3ec2b90cc19597c516817bd06b522225f
ADHD-Donkey <roden.gleb@gmail.com>
2026-06-18 09:47:32 +0300

    router

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,40 +1,28 @@
-import { createBrowserRouter, Outlet } from "react-router-dom";
+import {createBrowserRouter, Outlet} from "react-router-dom";
 import LoginRegisterPage from "../features/auth/pages/LoginRegisterPage";
 import ProfilePage from "../features/profile/pages/ProfilePage";
-import { useNotificationsWebSocket } from "../shared/hooks/useNotificationsWebSocket";
-
-import { useAuth } from "../shared/context/AuthContext";
+import {useNotificationsWebSocket} from "../shared/hooks/useNotificationsWebSocket";
+import RecommendationsPage from "../features/recommendations/pages/RecommendationsPage";
+import {useAuth} from "../shared/context/AuthContext";
 
 const RootLayout = () =>
 {
-    const { currentUserId } = useAuth();
+    const {currentUserId} = useAuth();
 
     useNotificationsWebSocket(currentUserId);
-import RecommendationsPage from "../features/recommendations/pages/RecommendationsPage";
 
-    return (
-        <>
-            <Outlet />
-        </>
-    );
+
+    return (<>
+        <Outlet/>
+    </>);
 };
 
-export const router: any = createBrowserRouter([
-    {
-        element: <RootLayout />,
-        children: [
-            {
-                path: "/",
-                element: <LoginRegisterPage />,
-            },
-            {
-                path: "/profile",
-                element: <ProfilePage />,
-            },
-            {
-                path: "/recommendations",
-                element: <RecommendationsPage />,
-            },
-        ]
-    }
-]);
+export const router: any = createBrowserRouter([{
+    element: <RootLayout/>, children: [{
+        path: "/", element: <LoginRegisterPage/>,
+    }, {
+        path: "/profile", element: <ProfilePage/>,
+    }, {
+        path: "/recommendations", element: <RecommendationsPage/>,
+    },]
+}]);
